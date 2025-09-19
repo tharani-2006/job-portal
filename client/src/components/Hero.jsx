3f24f66cd3f184f9a4e1edca34fa96bcc1ebd313
THARANIPRAKASH <tharaniprakashp2006@gmail.com>
2025-09-19 15:37:16 +0530

    add job listing feat

--- client/src/components/Hero.jsx
+++ client/src/components/Hero.jsx
@@ -1,7 +1,26 @@
-import React from 'react'
+import React, { useContext,useRef } from 'react'
 import { assets } from '../assets/assets'
+import { AppContext } from '../context/AppContext'
 
 const Hero = () => {
+
+    const {setSearchFilter,setIsSearched} = useContext(AppContext)
+
+    const titleRef = useRef(null)
+    const locationRef = useRef(null)
+
+    const onSearch =() =>{
+        setSearchFilter({
+            title:titleRef.current.value,
+            location:locationRef.current.value
+        })
+        setIsSearched(true)
+        console.log({
+            title:titleRef.current.value,
+            location:locationRef.current.value
+        });
+    }
+
     return (
         <div className='container 2xl:px-20 mx-auto my-10'>
             <div className='bg-gradient-to-r from-purple-800 to-purple-950 text-white py-16 text-center mx-2 rounded-xl'>
@@ -12,15 +31,17 @@ const Hero = () => {
                         <img className='h-4 sm:h-5' src={assets.search_icon} alt="" />
                         <input type="text"
                             placeholder='Search for jobs'
-                            className='max-sm:text-xs p-2 rounded outline-none w-full bg-white' />
+                            className='max-sm:text-xs p-2 rounded outline-none w-full bg-white' 
+                            ref={titleRef} />
                     </div>
                     <div className='flex items-center'>
                         <img className='h-4 sm:h-5' src={assets.location_icon} alt="" />
                         <input type="text"
                             placeholder='Location'
-                            className='max-sm:text-xs p-2 rounded outline-none w-full bg-white' />
+                            className='max-sm:text-xs p-2 rounded outline-none w-full bg-white' 
+                            ref={locationRef} />
                     </div>
-                    <button className='bg-blue-600 px-6 py-2 rounded text-white m-1'>Search</button>
+                    <button onClick={onSearch} className='bg-blue-600 px-6 py-2 rounded text-white m-1'>Search</button>
                 </div>
             </div>
 
---
+++ client/src/components/JobListing.jsx
@@ -0,0 +1,80 @@
+import React, { useContext } from 'react'
+import { AppContext } from '../context/AppContext'
+import { assets, JobCategories, JobLocations } from '../assets/assets'
+
+const JobListing = () => {
+
+    const { isSearched, searchFilter, setSearchFilter } = useContext(AppContext)
+
+    return (
+        <div className='container 2xl:px-20 mx-auto flex flex-col lg:flex-row max-lg:space-y-8 py-8'>
+
+            {/* Sidebar */}
+            <div className='w-full lg:w-1/4 bg-white px-4'>
+
+                {/* Search filter from Hero Component */}
+                {
+                    isSearched && (searchFilter.title !== "" || searchFilter.location !== "") && (
+                        <>
+                            <h3 className='font-medium text-lg mb-4'>Currect Search</h3>
+                            <div className='mb-4 text-gray-600'>
+                                {searchFilter.title && (
+                                    <span className='inline-flex items-center gap-2.5 bg-blue-50 border-blue-500 px-4 py-1.5 rounded border'>
+                                        {searchFilter.title}
+                                        <img onClick={e => setSearchFilter(prev => ({ ...prev, title: "" }))} className='cursor-pointer' src={assets.cross_icon} alt="" />
+                                    </span>
+                                )}
+                                {searchFilter.location && (
+                                    <span className='ml-2 inline-flex items-center gap-2.5 bg-red-50 border-red-500 px-4 py-1.5 rounded border'>
+                                        {searchFilter.location}
+                                        <img onClick={e => setSearchFilter(prev => ({ ...prev, location: "" }))} className='cursor-pointer' src={assets.cross_icon} alt="" />
+                                    </span>
+                                )}
+                            </div>
+                        </>
+                    )
+                }
+                {/* Category Filter */}
+                <div className='max-lg:hidden'>
+                    <h4 className='font-medium text-lg py-4'>Search By Categories</h4>
+                    <ul className='space-y-4 text-gray-600'>
+                        {
+                            JobCategories.map((category,index)=>(
+                                <li className='flex gap-3 items-center' key={index}> 
+                                    <input className='scale-125' type="checkbox" name="" id="" />
+                                    {category }
+                                </li>
+                            ))
+                        }
+                    </ul>
+                </div>
+                {/* Location Filter */}
+                <div className='max-lg:hidden'>
+                    <h4 className='font-medium text-lg py-4 pt-14'>Search By Location</h4>
+                    <ul className='space-y-4 text-gray-600'>
+                        {
+                            JobLocations.map((location,index)=>(
+                                <li className='flex gap-3 items-center' key={index}> 
+                                    <input className='scale-125' type="checkbox" name="" id="" />
+                                    {location }
+                                </li>
+                            ))
+                        }
+                    </ul>
+                </div>
+            </div>
+
+            {/* Job Listings */}
+            <section className='w-full lg:w-3/4 text-gray-800 max-lg:px-4'>
+                <h3 className='font-medium text-3xl py-2' id='job-list'>Latest Jobs</h3>
+                <p className='mb-8'>Get your desired job from top companies</p>
+                <div className='grid grid-cols-1 sm:grid-cols-2 xl:grid-cols-3 gap4'>   
+
+                </div>
+            </section>
+
+        </div>
+    )
+}
+
+export default JobListing
