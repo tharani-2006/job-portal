b9277a67f6245b65d140c91372213562af25f6ed
THARANIPRAKASH <tharaniprakashp2006@gmail.com>
2025-09-19 11:45:22 +0530

    feat : complete implement of navbar

---
+++ client/src/components/Hero.jsx
@@ -0,0 +1,15 @@
+import React from 'react'
+import { assets } from '../assets/assets'
+
+const Hero = () => {
+  return (
+    <div>
+        <div>
+        
+        </div>
+      
+    </div>
+  )
+}
+
+export default Hero
