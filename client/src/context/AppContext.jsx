0468badda29c057c32159e015513ad1b979cbdf7
THARANIPRAKASH <tharaniprakashp2006@gmail.com>
2025-09-19 13:47:19 +0530

    feat : add hero section

--- client/src/context/AppContext.jsx
+++ client/src/context/AppContext.jsx
@@ -1,14 +1,12 @@
-import { createContext } from "react";
+import { createContext, useState } from "react";
 
 
 export const AppContext = createContext()
 
 export const AppContextProvider = (props) => {
-     const value = {
 
-     }
-
-     return (<AppContext.Provider value={value}>
-        {props.children}
-     </AppContext.Provider>)
+  
+   return (<AppContext.Provider value={value}>
+      {props.children}
+   </AppContext.Provider>)
 }
