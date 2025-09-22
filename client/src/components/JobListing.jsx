0beb833eacebe8279af316453c5670d6321d90f1
THARANIPRAKASH <tharaniprakashp2006@gmail.com>
2025-09-22 09:37:33 +0530

    add toggle button for filters in small devide and pagination buttons

--- client/src/components/JobListing.jsx
+++ client/src/components/JobListing.jsx
@@ -1,4 +1,4 @@
-import React, { useContext } from 'react'
+import React, { useContext, useState } from 'react'
 import { AppContext } from '../context/AppContext'
 import { assets, JobCategories, JobLocations } from '../assets/assets'
 import JobCard from './JobCard'
@@ -6,6 +6,10 @@ import JobCard from './JobCard'
 const JobListing = () => {
 
     const { isSearched, searchFilter, setSearchFilter,jobs } = useContext(AppContext)
+
+    const [showFilter,setShowFilter] =useState(true)
+
+    const [currentPage,setCurrentPage] = useState(1)
 
     return (
         <div className='container 2xl:px-20 mx-auto flex flex-col lg:flex-row max-lg:space-y-8 py-8'>
@@ -35,8 +39,13 @@ const JobListing = () => {
                         </>
                     )
                 }
+
+                <button onClick={ e => setShowFilter(prev => !prev)} className='px-6 py-1.5 rounded border border-gray-400 lg:hidden'>
+                    {showFilter ? "Close" : "Filters"}
+                </button>
+
                 {/* Category Filter */}
-                <div className='max-lg:hidden'>
+                <div className={showFilter ? "" : "max-lg:hidden"}>
                     <h4 className='font-medium text-lg py-4'>Search By Categories</h4>
                     <ul className='space-y-4 text-gray-600'>
                         {
@@ -51,7 +60,7 @@ const JobListing = () => {
                 </div>
 
                 {/* Location Filter */}
-                <div className='max-lg:hidden'>
+                <div className={showFilter ? "" : "max-lg:hidden"}>
                     <h4 className='font-medium text-lg py-4 pt-14'>Search By Location</h4>
                     <ul className='space-y-4 text-gray-600'>
                         {
@@ -75,6 +84,24 @@ const JobListing = () => {
                         <JobCard key={index} job={job} />
                     ))}
                 </div>
+
+                    {/* Pagination */}
+                    {jobs.length > 0 && (
+                        <div className='flex items-center justify-center space-x-2 mt-10'>
+                            <a href='#job-list'>
+                                <img src={assets.left_arrow_icon} alt="" />
+                            </a>
+                            {Array.from({length:Math.ceil(jobs.length/6)}).map((_,index) => (
+                                <a href='#job-list'>
+                                    <button className={`w-10 h-10 flex items-center justify-center border border-gray-300 rounded`}>{index+1}</button>
+                                </a>
+                            ))}
+                            <a href='#job-list'>
+                                <img src={assets.right_arrow_icon} alt="" />
+                            </a>
+                        </div>
+                    )}
+
             </section>
 
         </div>
