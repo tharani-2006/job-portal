ab1f08db10613bc19424b243785c27e0221f6fa6
THARANIPRAKASH <tharaniprakashp2006@gmail.com>
2025-11-11 15:44:45 +0530

    implement routes for complete project

---
+++ server/controllers/companyController.js
@@ -0,0 +1,14 @@
+//Register a new company
+
+//company login
+
+//get company data
+
+//post a new job
+
+//get comapny job applicants
+
+//get company posted jobs
+
+//change job application status
+
---
+++ server/routes/companyRoutes.js
@@ -0,0 +1,37 @@
+import express from 'express'
+import { registerCompany } from '../controllers/companyController.js'
+import { loginCompany } from '../controllers/companyController.js'
+import { getCompanyData } from '../controllers/companyController.js'
+import { postJob } from '../controllers/companyController.js'
+import { getCompanyJobApplicants } from '../controllers/companyController.js'
+import { getCompanyPostedJobs } from '../controllers/companyController.js'
+import { changeJobApplicationStatus } from '../controllers/companyController.js'
+import { changeVisibility } from '../controllers/companyController.js'
+
+const router = express.Router()
+
+//register a new company
+router.post('/register', registerCompany)
+
+//company login
+router.post('/login', loginCompany)
+
+//get company data
+router.get('/company-data', getCompanyData)
+
+//post a new job
+router.post('/post-job', postJob)
+
+//get applicants Data of company   
+router.get('/applicants', getCompanyJobApplicants)
+
+//get company job list
+router.get('/list-jobs', getCompanyPostedJobs)
+
+//change application status
+router.put('/change-status', changeJobApplicationStatus)
+
+//change application visibility
+router.put('/change-visibility', changeVisibility)
+
+export default router
