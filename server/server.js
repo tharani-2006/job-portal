283bcfb6ecacc14bbba2fb02684186261adef60d
THARANIPRAKASH <tharaniprakashp2006@gmail.com>
2025-11-12 05:57:50 +0530

    setup cloudinary storage and jwt generation

--- server/server.js
+++ server/server.js
@@ -6,12 +6,14 @@ import connectDB from './config/db.js'
 import * as Sentry from "@sentry/node";
 import { clerkWebhooks } from './controllers/webhook.js'
 import companyRoutes from './routes/companyRoutes.js'
+import connectCloudinary from './config/cloudinary.js'
 
 // Initialize Express
 const app = express()
 
 // connect to database
 await connectDB()
+await connectCloudinary()
 
 //Middlewares
 app.use(cors())
